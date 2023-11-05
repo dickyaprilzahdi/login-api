e69ad9bdab98511fdf968129eb64dbec1c229f4e
Dicky April Zahdi <credentialreuse@gmail.com>
2023-11-06 00:07:03 +0700

    Koneksi ke Database dan Membuat Table

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 import express from "express";
+import db from "./config/Database.js";
+import Users from "./models/UserModel.js";
 
 const app = express();
+
+// Kondisi database
+try {
+    await db.authenticate();
+    console.log('Database terkoneksi dengan baik...');
+
+    // await Users.sync(); -> JIka belum ada table di database
+} catch (error) {
+    console.log(error);
+}
 
 app.listen(5000, ()=> console.log('Server berjalan dengan normal di port 5000'));
