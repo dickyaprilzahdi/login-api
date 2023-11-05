7ded46d6bb6ba38afc783d036f7fa04fbfbf0fd2
Dicky April Zahdi <credentialreuse@gmail.com>
2023-11-06 00:25:06 +0700

    Menampilkan user berhasil

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/Database.js";
 import Users from "./models/UserModel.js";
+import router from "./routes/index.js";
 
 const app = express();
 
@@ -13,5 +14,8 @@ try {
 } catch (error) {
     console.log(error);
 }
+
+app.use(express.json()); // Menerima data json
+app.use(router); // Sebagai middleware
 
 app.listen(5000, ()=> console.log('Server berjalan dengan normal di port 5000'));
